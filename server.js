ffe45c30e677aa5ac2015e2a1655fe152794bdaa
HayZeusCrust <jpowalow@gmail.com>
2024-10-02 12:35:45 -0400

    Added git ignore and setup mongodb

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
 //DEPENDENCIES
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
 
 //APP SETUP
@@ -10,6 +15,12 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
 app.use('/', indexRouter);
 
 app.use(logger);
